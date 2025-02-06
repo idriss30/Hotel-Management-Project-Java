ee540a83fa372da2018f2663d547458730f2bec6
idriss30 <idriss.cissoko@hotmail.com>
2025-02-06 09:30:30 -0500

    javadoc generated

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"src","l":"Doubleroom"},{"p":"src","l":"Food"},{"p":"src","l":"Holder"},{"p":"src","l":"Hotel"},{"p":"src","l":"Main"},{"p":"src","l":"NotAvailable"},{"p":"src","l":"SingleRoom"},{"p":"src","l":"Write"}];updateSearchResults();
